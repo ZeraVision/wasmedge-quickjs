a067a391dd996a0d5aae857248e2f2e05ae17021
IKhomZera <IKhom@zera.vision>
2023-10-04 11:04:55 +0300

    allow to pass code instead of file

--- src/main.rs
+++ src/main.rs
@@ -5,36 +5,30 @@ use wasmedge_quickjs::*;
 
 fn args_parse() -> (String, Vec<String>) {
     use argparse::ArgumentParser;
-    let mut file_path = String::new();
+    let mut code = String::new();
     let mut res_args: Vec<String> = vec![];
     {
         let mut ap = ArgumentParser::new();
-        ap.refer(&mut file_path)
-            .add_argument("file", argparse::Store, "js file")
+        ap.refer(&mut code)
+            .add_argument("code", argparse::Store, "js code")
             .required();
         ap.refer(&mut res_args)
             .add_argument("arg", argparse::List, "arg");
         ap.parse_args_or_exit();
     }
-    (file_path, res_args)
+    (code, res_args)
 }
 
 fn main() {
     use wasmedge_quickjs as q;
     let mut rt = q::Runtime::new();
     rt.run_with_context(|ctx| {
-        let (file_path, mut rest_arg) = args_parse();
-        let code = std::fs::read_to_string(&file_path);
-        match code {
-            Ok(code) => {
-                rest_arg.insert(0, file_path.clone());
-                ctx.put_args(rest_arg);
-                ctx.eval_module_str(code, &file_path);
-            }
-            Err(e) => {
-                eprintln!("{}", e.to_string());
-            }
-        }
+        let (code, mut rest_arg) = args_parse();
+        
+        rest_arg.insert(0, code.clone());
+        ctx.put_args(rest_arg);
+        ctx.eval_module_str(code, "");
+
         ctx.js_loop().unwrap();
     });
 }
